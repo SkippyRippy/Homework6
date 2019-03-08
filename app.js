7acf176023643c28a581519dd9ffb8eb39289526
SkippyRippy <aj.aquacats@gmail.com>
2019-03-08 01:05:21 -0600

    woohoo

---
+++ app.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+    var topics = [
+        "pikachu",
+        "bulbasaur",
+        "charmander",
+        "squirtle"
+    ];
+
+    var renderButtons = function() {
+        for (i = 0; i < topics.length; i++) {
+            $("#button1").append(`<button class="btn btn-sm btn-dark giphy-button" data-subject="${topics[i]}">${topics[i]}</button>`)
+        }
+    }
+
+    renderButtons();
+
+    // clicking/submitting the #magic-maker
+    $("#magic").click(function() {
+        event.preventDefault();
+        var newTopic = $("#topic").val().trim();
+        topics.push(newTopic);
+        $("#button1").empty();
+        renderButtons();
+
+    })
+
+    // clicking any giphy buttons
+    $("#button1").on("click", ".giphy-button", function () {
+        $("#gif-catcher").empty();
+        var giphySubject = $(this).attr("data-subject"),
+            queryURL = `https://api.giphy.com/v1/gifs/search?q=${giphySubject}&api_key=dc6zaTOxFJmzC&limit=10`;
+        console.log(queryURL)
+
+        //AJAX request to giphy
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            for (i = 0; i <= 9; i++) {
+                $("#gif-catcher").append(`<div class="each-gif"><img src="${response.data[i].images.fixed_height_still.url}" data-still="${response.data[i].images.fixed_height_still.url}" data-animate="${response.data[i].images.fixed_height.url}" data-state="still" class="gif" alt="${response.data[i].title}"><p>Rating: ${response.data[i].rating.toUpperCase()}</p></div>`);
+            }
+        })
+    });
+
+    // click a gif to animate, or make still
+    $("#gif-catcher").on("click", "img", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+
+});
